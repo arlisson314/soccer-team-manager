31e824723219e0389c30812d07c80adcc9bab687
Arlisson N. Costa <arlisson314@gmail.com>
2023-04-04 21:44:54 -0300

    feat: cria rotas  '/' e '/teams',

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const teams = require('./mock');
 
 const app = express();
+app.use(express.json());
+
+app.get('/', (_req, res) => res.status(200).json({ message: 'Olá mundo!' }));
+app.get('/teams', (_req, res) => res.status(200).json({ teams }));
+app.post('/teams', (req, res) => {
+  const newTeam = req.body;
+  teams.push(newTeam);
+  res.status(201).json({ message: 'Time adicionado com sucesso!' });
+});
 
 module.exports = app;
